41cedfb600b68cc300d4a486d3a78c4b4c02acba
ZecharyDouglas <Zechary.Douglas@cix.csi.cuny.edu>
2026-01-27 19:41:44 -0500

    Second Commit

--- Personal-Website/src/App.jsx
+++ Personal-Website/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
 
         {/* Content goes on top */}
         <div className="flex flex-col min-h-dvh relative z-10 ">
-          <div className="flex relative z-10 pt-10 px-10 text-[#605e5e] justify-end ">
+          <div className="flex items-center relative z-10 pt-10 px-10 text-[#605e5e] justify-end ">
             <button className=" border p-2 rounded-md mx-3  font-raleway text-2xl font-bold hover:text-[#ad96d1] transition">
               Home
             </button>
@@ -35,11 +35,11 @@ function App() {
             </button>
           </div>
           <div>
-            <div className="h-px w-full bg-[#605e5e]" />
+            <div className="h-px w-full bg-[#605e5e] shadow-lg" />
           </div>
           {/* Hero Card */}
           <div className="flex justify-center p-10">
-            <div className="h-[80vh] w-[90vw] rounded-xl bg-[#7649BF]/25 shadow-lg">
+            <div className="h-[80vh] w-[100vw] rounded-xl bg-black shadow-lg">
               <div className="grid grid-cols-4 grid-rows-4 place-items-center">
                 <div className=" col-span-3 row-span-4 flex flex-col">
                   <div className=" flex flex-col justify-center items-center font-raleway">
@@ -71,7 +71,7 @@ function App() {
           </div>
           {/* About Me Card */}
           <div className="flex justify-center p-10">
-            <div className="h-[80vh] w-[90vw] rounded-xl bg-[#7649BF]/25 shadow-lg">
+            <div className="h-[80vh] w-[90vw] rounded-xl bg-[#7649BF]/25 shadow-lg p-10">
               <div className="">
                 <div className="flex flex-col">
                   <div className=" flex flex-col justify-center items-start pl-5 font-raleway">
@@ -83,10 +83,14 @@ function App() {
                         of Science in Computer Science.
                       </h2>
                       <h2 className="text-white text-2xl">
-                        Based out of Brooklyn, NYC
+                        I develop web applications using a variety of
+                        technologies ranging from React.js front end to Flask
+                        and GraphQl backend. Some of my most recent
                       </h2>
                       <h2 className="text-white text-2xl">
-                        Let's build together!
+                        Work includes a Social messenging app called Whisper
+                        that leverages a GraphQl websocket libarary to
+                        facilitate real-time duplex communication between users.
                       </h2>
                     </div>
                     <div className="flex">
@@ -101,7 +105,7 @@ function App() {
                           />
                         </a>
                       </div>
-                      <div className="flex justify-center">
+                      <div className="flex justify-center p-5">
                         <a href="https://www.parchment.com/u/award/469d5f329fde907ae2c22a9616bcc0a6">
                           <img
                             src={Diploma}
@@ -112,7 +116,7 @@ function App() {
                           />
                         </a>
                       </div>
-                      <div className="flex justify-center">
+                      <div className="flex justify-center p-5">
                         <a href="https://www.csi.cuny.edu/academics-and-research/departments-programs/computer-science">
                           <img
                             src={CSILogo}
@@ -131,21 +135,22 @@ function App() {
           </div>
           {/* Certifications / Projects Card */}
           <div className="flex justify-center p-10">
-            <div className="h-[80vh] w-[90vw] rounded-xl bg-[#7649BF]/25 shadow-lg">
+            <div className="h-[80vh] w-[90vw] rounded-xl bg-[#7649BF]/25 shadow-lg p-10">
               <div className="grid grid-cols-4 grid-rows-4 place-items-center">
                 <div className=" col-span-3 row-span-4 flex flex-col">
                   <div className=" flex flex-col justify-center items-center font-raleway">
                     <h2 className="text-white text-7xl text-left">
                       Certifications & Projects
                     </h2>
-                    <h2 className="text-white text-2xl text-left">
-                      Passionate Full-Stack Web developler{" "}
+                    <h2 className="text-white text-2xl">
+                      I develop web applications using a variety of technologies
+                      ranging from React.js front end to Flask and GraphQl
+                      backend. Some of my most recent
                     </h2>
                     <h2 className="text-white text-2xl">
-                      Based out of Brooklyn, NYC
-                    </h2>
-                    <h2 className="text-white text-2xl">
-                      Let's build together!
+                      Work includes a Social messenging app called Whisper that
+                      leverages a GraphQl websocket libarary to facilitate
+                      real-time duplex communication between users.
                     </h2>
                     <div className="rounded-xl bg-[#7649BF]/25 shadow-lg h-100 w-100 px-15 pb-20">
                       <img
